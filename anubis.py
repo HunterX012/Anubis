9075c2dab86c14645263fb6f404c5be7cee67967
HunterX012 <114446800+HunterX012@users.noreply.github.com>
2024-05-01 22:44:41 -0400

    Update anubis.py

--- anubis.py
+++ anubis.py
@@ -478,7 +478,7 @@ print(blue(f"        [>] Code has been successfully obfuscated @ {name}"), end="
 if extra == False:
     compile = False
     while True:
-        ans = "n"
+        ans = "y"
         if ans == "y":
             compile = True
             break
